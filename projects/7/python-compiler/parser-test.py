bf4051500e95728584037929f03cdd03de1b57d9
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-04-29 14:16:22 +0200

    test: adding parser test file

---
+++ projects/7/python-compiler/parser-test.py
@@ -0,0 +1,37 @@
+import unittest
+from parser import Parser
+
+
+class TestParser(unittest.TestCase):
+
+    def test_has_more_lines(self):
+        parser = Parser('SimpleAdd.vm')
+        self.assertTrue(parser.has_more_lines())  # First line
+        self.assertTrue(parser.has_more_lines())  # Second line
+        self.assertTrue(parser.has_more_lines())  # Third line (should be empty, but still exists)
+        self.assertFalse(parser.has_more_lines())  # EOF reached
+        self.assertFalse(parser.has_more_lines())  # EOF reached
+
+    def test_advance(self):
+        parser = Parser('SimpleAdd.vm')
+
+        # First line
+        parser.advance()
+        self.assertEqual(parser.command_type, 'push')
+        self.assertEqual(parser.arg1, 'constant')
+        self.assertEqual(parser.arg2, '7')
+        # Second line
+        parser.advance()
+        self.assertEqual(parser.command_type, 'push')
+        self.assertEqual(parser.arg1, 'constant')
+        self.assertEqual(parser.arg2, '8')
+
+        # Third line
+        parser.advance()
+        self.assertEqual(parser.command_type, 'add')
+        self.assertEqual(parser.arg1, None)
+        self.assertEqual(parser.arg2, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- projects/7/python-compiler/parser.py
+++ projects/7/python-compiler/parser.py
@@ -5,15 +5,18 @@ class Parser:
         self.current_line = None
         self.arg1 = None
         self.arg2 = None
+        self.command_type = None
         self.valid_commands = ['push', 'add']
 
     def has_more_lines(self):
+        if self.file.closed:
+            return False
         next_line = self.file.readline()
         if next_line:
             self.current_line = next_line
             return True
         else:
-            print('else')
+            self.file.close()
             return False
 
     def advance(self):
@@ -23,15 +26,22 @@ class Parser:
                     words_of_line = self.current_line.split()
                     command = words_of_line[0]
                     if command not in self.valid_commands:
-                        raise Exception('Invalid command')
-                except Exception as error:
-                    print('An error occurred: ', error)
+                        print('Invalid command')
+                        self.command_type = None
+                        raise ValueError
+                    else:
+                        self.command_type = command
+                except ValueError:
+                    print('An error occurred: invalid command')
                 try:
                     if command != 'add':
                         self.arg1 = words_of_line[1]
                         self.arg2 = words_of_line[2]
-                except:
-                    print('An error occurred: ', 'Missing arguments')
+                    else:
+                        self.arg1 = None
+                        self.arg2 = None
+                except IndexError:
+                    print('Missing arguments')
 
     def arg1(self):
         return self.arg1
@@ -39,5 +49,7 @@ class Parser:
     def arg2(self):
         return self.arg2
 
+    def command_type(self):
+        return self.command_type
 
 
