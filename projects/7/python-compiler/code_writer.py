bf832260caf0ca687a0f6e364ba1914915f7206a
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-05-06 21:12:00 +0200

    feat: PointerTest.vm fixed with correct code

--- projects/7/python-compiler/code_writer.py
+++ projects/7/python-compiler/code_writer.py
@@ -17,7 +17,7 @@ class CodeWriter:
         self.push_value_of_d_to_stack_pointer = '@SP\nA=M\nM=D\n@SP\nM=M+1'
         self.set_d_to_value_at_pointer = 'A=M\nD=M'
         self.pop_stack_pointer_value_to_d = '@SP\nM=M-1\n@SP\nA=M\nD=M'
-        self.address_dict = {'local': '@LCL', 'that': '@THAT', 'argument': '@ARG', 'this': '@THIS'}
+        self.address_dict = {'local': '@LCL', 'that': '@THAT', 'argument': '@ARG', 'this': '@THIS', '3': '@3', '4': '@4'}
         self.temp_dict = {
             '0': '@5',
             '1': '@6',
@@ -28,6 +28,9 @@ class CodeWriter:
             '6': '@11',
             '7': '@12'
         }
+        self.static_counter = 0
+        self.current_this = 3
+        self.current_that = 4
 
     def file_writer(self, code_to_write):
         with open(self.output_file_name, "a") as file:
@@ -36,10 +39,14 @@ class CodeWriter:
                 file.write("\n")
 
     def get_push_code_line(self, arg1, arg2):
+        if arg1 == 'that':
+            this_base_address = '@' + str(self.current_that)
         if arg1 == 'temp':
             return [self.temp_dict[arg2], 'D=M', self.push_value_of_d_to_stack_pointer]
         if arg1 == 'constant':
             return ['@' + arg2, 'D=A', self.push_value_of_d_to_stack_pointer]
+        if 'Static' in arg1:
+            return [arg1, 'D=A', self.push_value_of_d_to_stack_pointer]
         else:
             r_13_to_final_address = ['@' + arg2, 'D=A', self.address_dict[arg1], 'A=M', 'D=D+A', '@R13', 'M=D']
             set_final_address_to_arg2 = ['@R13', self.set_d_to_value_at_pointer, self.push_value_of_d_to_stack_pointer]
@@ -49,8 +56,12 @@ class CodeWriter:
         return [self.pop_stack_pointer_value_to_d, self.temp_dict[arg2], 'M=D']
 
     def get_pop_code_line(self, arg1, arg2):
-        r_13_to_final_address = ['@' + arg2, 'D=A', self.address_dict[arg1], 'A=M', 'D=D+A', '@R13', 'M=D']
-        set_final_address_to_arg2 = [self.set_pointer_down, self.SP, self.set_d_to_value_at_pointer, '@R13', 'A=M', 'M=D']
+        address_to_write = arg1
+        if 'Static' not in arg1:
+            address_to_write = self.address_dict[arg1]
+        r_13_to_final_address = ['@' + arg2, 'D=A', address_to_write, 'A=M', 'D=D+A', '@R13', 'M=D']
+        set_final_address_to_arg2 = [self.set_pointer_down, self.SP, self.set_d_to_value_at_pointer, '@R13', 'A=M',
+                                     'M=D']
         return r_13_to_final_address + set_final_address_to_arg2
 
     def write_arithmetic(self, operation):
@@ -74,7 +85,14 @@ class CodeWriter:
         code_to_write = []
         if operation == 'push':
             if arg1 == 'pointer':
-                target = self.pointer_target(arg2)
+                if arg2 == '0':
+                    code_to_write = ['@3', 'D=M', self.push_value_of_d_to_stack_pointer]
+                elif arg2 == '1':
+                    code_to_write = ['@4', 'D=M', self.push_value_of_d_to_stack_pointer]
+                else:
+                    raise ValueError(f'{arg2} is not a valid number for pointer, should be either "1" or "0"')
+            elif arg1 == 'static':
+                target = f'@StaticTest.{self.static_counter}'
                 code_to_write = self.get_push_code_line(target, '0')
             else:
                 code_to_write = self.get_push_code_line(arg1, arg2)
@@ -82,8 +100,20 @@ class CodeWriter:
             if arg1 == 'temp':
                 code_to_write = self.get_pop_temp(arg2)
             elif arg1 == 'pointer':
-                target = self.pointer_target(arg2)
-                code_to_write = self.get_pop_code_line(target, '0')
+                if arg2 == '0':
+                    self.current_this = arg2
+                    code_to_write = [self.pop_stack_pointer_value_to_d, '@3', 'M=D']
+                elif arg2 == '1':
+                    self.current_that = arg2
+                    code_to_write = [self.pop_stack_pointer_value_to_d, '@4', 'M=D']
+                else:
+                    raise ValueError(f'{arg2} is not a valid number for pointer, should be either "1" or "0"')
+            elif arg1 == 'static':
+                target = f'@StaticTest.{self.static_counter}'
+                code_to_write = self.get_pop_code_line(target, arg2)
+                self.static_counter += 1
+                if self.static_counter >= 240:
+                    raise Exception('More than 240 static variables')
             else:
                 code_to_write = self.get_pop_code_line(arg1, arg2)
 
